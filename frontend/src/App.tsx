f5d717a6495cfe17b72e854db8e4360aab5103dc
slaveeks <chernyshev2001@icloud.com>
2022-06-24 18:49:25 +0300

    Initial commit, added base for project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from './components/layouts/Container';
+
+/**
+ * Makes the main page
+ *
+ * @return {JSX.Element}
+ */
+function App(): JSX.Element {
+  return (
+    <Container>
+    </Container>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/layouts/Container.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/**
+ * Interface for container component props
+ */
+interface Props{
+}
+
+/**
+ * Styled container component
+ *
+ * @param props - props of component
+ */
+const ContainerStyled = styled.div<Props>`
+  display: flex;
+  position: absolute;
+  height: 100vh;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 100vw;
+`;
+
+/**
+ * Container component
+ *
+ * @param {Props} props - props of component
+ *
+ * @return {React.FC<Props>}
+ */
+const Container: React.FC<Props> = ({...props}) => {
+  return (
+    <ContainerStyled {...props}/>
+  );
+};
+
+export default Container;
